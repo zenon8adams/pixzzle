5073d5d8868c55ad747bd978beea6181d33d571c
Meekness Adesina <zenon8adams@gmail.com>
2023-11-02 23:29:17 +0100

    Source/Minor: Build separate thumbnail view
    The UI has been redesigned with a new idea of
    thumbnails. Each shots taken are stored
    in the `USER_CACHE_DIR` which resolves
    to ~/.cache on debian. These are loaded into
    a viewport which displays a tiny thumbnail.
    Any of the entries in the side view when
    clicked, expands into a bigger view pane.
    
    Upcoming:
        - Implement a smarter expansion mechanism
          for the side view. Currently, the
          view expands in an horrible manner.
        - Improve hacks in code.
    
    Signed-off-by: Meekness Adesina <zenon8adams@gmail.com>

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-const {Gio} = imports.gi;
-    
+const { Gio, GLib } = imports.gi;
+
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
@@ -14,16 +14,42 @@ function lg() {
 }
 
 function format() {
-   const vicinity = arguments[0];
-   const actualArgs = Array.from(arguments).slice(1);
-   return '[' + SCHEMA_NAME + '] ' + '[' + vicinity + '] ' + actualArgs.join(' ');
+  const vicinity = arguments[0];
+  const actualArgs = Array.from(arguments).slice(1);
+  return (
+    '[' + SCHEMA_NAME + '] ' + '[' + vicinity + '] ' + actualArgs.join(' ')
+  );
 }
 
 function inflateSettings() {
   let schemaDir = Me.dir.get_child('schemas').get_path();
   let schemaSource = Gio.SettingsSchemaSource.new_from_directory(
-      schemaDir, Gio.SettingsSchemaSource.get_default(), false);
+    schemaDir,
+    Gio.SettingsSchemaSource.get_default(),
+    false
+  );
   let schema = schemaSource.lookup(SCHEMA_NAME, false);
 
-  return new Gio.Settings({settings_schema : schema});
+  return new Gio.Settings({ settings_schema: schema });
+}
+
+const _getShotStore = function () {
+  const path = Gio.File.new_for_path(
+    GLib.build_filenamev(Array.from(arguments))
+  );
+  try {
+    path.make_directory_with_parents(null);
+  } catch (e) {
+    if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.EXISTS)) {
+      return null;
+    }
+  }
+
+  return path;
 };
+
+var SHOT_STORE = _getShotStore(
+  GLib.get_user_cache_dir(),
+  'extension',
+  SCHEMA_NAME
+);
